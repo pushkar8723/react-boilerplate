44213caf426399bd67786f43e9a7be14e1bff47f
Pushkar Anand <pushkar8723@gmail.com>
2018-07-18 02:17:20 +0530

    Hello world.

---
+++ README.md
@@ -0,0 +1,5 @@
+React Boilerplate
+=================
+
+My novice attempt to create a reusable boilerplate code for react projects.
+*Note:* This is still work in progress.
---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>Hello User!</div>,
+    document.getElementById("root")
+);
