9231ab9ba1390b84717d7fb0f64db833b2ee9562
Pushkar Anand <pushkar8723@gmail.com>
2018-07-19 08:56:55 +0530

    Documentation lint rule
    Added rule to mandate code documentation in both
    eslint and tslint.

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -3,10 +3,13 @@
  */
 export default class Common {
     /**
-     * Checks if user is authorized.
+     * Checks if user is authorized for the given role.
+     *
+     * @param {string}      requiredRole
+     * @param {string[]}    userRoles
+     * @returns {boolean}
      */
-    public static isAuthorized(): boolean {
-        // TODO: implement authentication rule.
-        return true;
+    public static isAuthorized(requiredRole: string, userRoles: string[]): boolean {
+        return (userRoles.indexOf(requiredRole) !== -1);
     }
 }
